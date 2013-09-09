894e24815a2e81d2e2c2e96145e2b1f88ca5e7b1
Diego <darojasv@gmail.com>
2013-09-08 18:17:05 -0600

    Archivo leeme

--- shpt01/shpt01/Program.cs
+++ shpt01/shpt01/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.SharePoint.Administration;
 
 namespace shpt01
 {
@@ -9,6 +10,18 @@ namespace shpt01
     {
         static void Main(string[] args)
         {
+
+        }
+
+        public static void ListarApps() 
+        {
+           SPWebApplicationCollection coleccionesapps= SPWebService.ContentService.WebApplications;
+           if (coleccionesapps.Count != null) {
+               foreach (var spwWebApp in coleccionesapps) 
+               {
+                   Console.WriteLine(spwWebApp.ApplicationPool.Name);
+               }
+           }
         }
     }
 }
